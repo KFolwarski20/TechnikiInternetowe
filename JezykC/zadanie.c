ca687427695c3b7574236e35bb8f42fc47319029
KFolwarski20 <90961384+KFolwarski20@users.noreply.github.com>
2022-03-12 09:46:48 +0100

    First trials

---
+++ JezykC/zadanie.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    char imie[20];
+    printf("Podaj swoje imie:\n");
+    scanf("%s", imie);
+    printf("Twoje imie to %s.\n", imie);
+    return 0;
+}
---
+++ pierwszy/pierwszy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pierwszy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
